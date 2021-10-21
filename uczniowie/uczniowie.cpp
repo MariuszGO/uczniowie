cc806293abe2fdab9336d032dc51e76e8693860b
Mariusz Gogola <mariuszgogola@gmail.com>
2021-10-21 09:53:45 +0200

    Dodaj pliki projektów.

---
+++ uczniowie/uczniowie.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "class_uczniowie.h"
+using namespace std;
+
+
+int main()
+{
+    
+    uczniowie *dane, *uczen = new uczniowie();
+
+
+    uczniowie* u1;
+    u1 = new uczniowie();
+
+    
+    //dane->wprowadz();
+   // dane->wyswietl();
+
+    uczen->imie = "Adam";
+    uczen->nazwisko = "Nowak";
+    uczen->wyswietl();
+    u1->wprowadz();
+    u1->wyswietl();
+
+
+}
+
